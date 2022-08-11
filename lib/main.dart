ad9397734f37c931b6da38bf439da8548a43d8d6
dabuzida <dabuzida@gmail.com>
2022-08-11 18:07:30 +0900

    1

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,18 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _text = 'bbbbbbbbbbbbbbbbbb';
+  @override
   Widget build(BuildContext context) {
+    print('build');
     return MaterialApp(
       title: 'Icon',
       home: Scaffold(
@@ -21,14 +28,36 @@ class MyApp extends StatelessWidget {
           centerTitle: true,
         ),
         body: Center(
-          child: Column(children: <Widget>[
-            Icon(Icons.block, color: Colors.red),
-            SizedBox(height: 50),
-            Container(
-              decoration: BoxDecoration(border: Border.all(color: Colors.green, width: 1)),
-              child: SizedBox.shrink(),
-            ),
-          ]),
+          child: Column(
+            children: <Widget>[
+              Icon(Icons.block, color: Colors.red),
+              SizedBox(height: 50),
+              Container(
+                decoration: BoxDecoration(border: Border.all(color: Colors.green, width: 1)),
+                child: SizedBox.shrink(),
+              ),
+              TextButton(
+                  onPressed: () async {
+                    print(1);
+                    setState(() {
+                      print(2);
+                    });
+                    print(3);
+                    _text = 'xxxxxxxxxxxxxx';
+                    print(4);
+                    Future.delayed(Duration(seconds: 2), () {
+                      print('delayed');
+                      _text = 'tttttttttttt';
+                      setState(() {
+                        print(5);
+                      });
+                      print(6);
+                    });
+                    print(7);
+                  },
+                  child: Text(_text, style: TextStyle(fontSize: 50))),
+            ],
+          ),
         ),
       ),
     );
